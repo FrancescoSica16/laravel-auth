3e6c106914ed56d034522d9dd7ef5493404afba4
FrancescoSica16 <francesco.sica16@gmail.com>
2021-11-10 18:48:44 +0100

    scaffolding + creaz HomeController + creazione PostController creaz view per posts

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,18 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::middleware('auth')->get('/admin', 'Admin/HomeController@index')->name('');
+Route::middleware('auth') //devi essere autenticato     
+->namespace('Admin')      // prrendi i controller delle route tue figlie a partire dalla cartella Admin/
+->prefix('admin')         // inserisci come prefisso nelle URI di tutte le route figlie di admin
+->name('admin.')          // inserisci come prefisso per ogni nome di tutte le route figlie "admin."
+->group(function(){       // e raggruppale in
 
-Route::get('/home', 'HomeController@index')->name('home');
+    // tutte le rotte che iniziano con prefisso " "
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('posts', 'PostController');
+    // Route::get('singlePost', 'HomeController@show')->name('singlePost');
+
+});
+// Route::middleware('auth')->get('/admin', 'Admin\HomeController@index')->name('admin.home');
+
+// Route::get('/home', 'HomeController@index')->name('home');
